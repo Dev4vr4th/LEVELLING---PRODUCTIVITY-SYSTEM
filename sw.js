60fa12a387eb9ff1f926020cf952771479751e80
Devavrath .k <devavrathk@gmail.com>
2026-07-10 19:30:30 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,8 +13,8 @@ const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
